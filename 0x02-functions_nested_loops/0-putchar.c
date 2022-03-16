3b790e051b4da72f24aae648dd65bdf7d52a2723
Abdulkarim <amahamn@gmail.com>
2022-03-16 23:45:42 +0100

    rr

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('B');
+	_putchar('e');
+	_putchar('t');
+	_putchar('t');
+	_putchar('y');
+	_putchar('\n');
+
+	return (0);
+}
