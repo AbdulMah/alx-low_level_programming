342f173dcb1ab631f10170b59ae0538f9c775fa4
Abdoul <80994637+AbdulMah@users.noreply.github.com>
2022-03-18 00:13:11 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
   */
 int main(void)
 {
-	_putchar('B');
-	_putchar('e');
-	_putchar('t');
-	_putchar('t');
-	_putchar('y');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putcgar('t');
+	_putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
 	_putchar('\n');
 
 	return (0);
